1853c38905ac110feab3232fec9128a87e2875d6
Malik Akbar Hashemi Rafsanjani <pro.malikakbar2357@gmail.com>
2021-08-11 00:47:38 +0700

    add fake email generator functionality

--- src/Email.js
+++ src/Email.js
@@ -1,14 +1,41 @@
 import './Email.css';
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const Email = () => {
   // CONTOH FUNGSI
   // const generator = () => {
   //   console.log("Ini fake email generator");
   // }
+  const [fakeEmail, setFakeEmail] = useState("");
+
+  const handleClipBoardClick = () => {
+    alert('Fake email copied to clipboard');
+    console.log("copied");
+  }
+
+  const updateEmail = () => {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const display = this.responseText.slice(2,this.responseText.length-2);
+        setFakeEmail(display);
+      }
+    };
+    xhttp.open("GET", "https://www.1secmail.com/api/v1/?action=genRandomMailbox", true);
+    xhttp.send();
+  }
 
   return (
     <div className="email">
       <h1>INI TEMP EMAIL</h1>
+      <span>{fakeEmail}</span>
+      <CopyToClipboard text={fakeEmail} onCopy={handleClipBoardClick}>
+        <button>Copy</button>
+      </CopyToClipboard>
+      <button onClick={updateEmail}>
+        Generate Fake Email
+      </button>
     </div>
   );
 }
