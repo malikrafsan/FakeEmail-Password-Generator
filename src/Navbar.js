a8c40ea80602aa751cf105c1f6e7a0a9bd301dd5
Grace Claudia <16520125@std.stei.itb.ac.id>
2021-08-13 16:25:08 +0700

    add navbar style

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,14 +4,15 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <h1>INI NAVBAR</h1>
-      <Link to="/email">EMAIL</Link>
-      <br />
-      <Link to="/password">PASSWORD</Link>
-      <br />
-      <Link to="/articles">ARTICLES</Link>
+      <ul>
+        {/* <li>The Secure Generator</li> */}
+      <p className="logo" style = {{color:"white"}}>The Secure Generator</p>
+        <li><Link to="/articles">Why it matters</Link></li>
+        <li><Link to="/email">Generate E-Mail</Link></li>
+        <li><Link to="/password">Generate Password</Link></li>
+      </ul>
     </div>
   );
 }
- 
+
 export default Navbar;
